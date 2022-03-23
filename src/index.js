0b8b6685129df610ff5e5b8130ceb409b16ccd85
AButsai <butsai@ukr.net>
2022-03-23 23:00:34 +0300

    feature/diet.html, diet.scss, fix barf.*

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './sass/main.scss';
 import './js/mobile-menu.js';
 import './js/barf-slider.js';
+import './js/diet-slider.js';
---
+++ src/js/diet-slider.js
@@ -0,0 +1,51 @@
+import Swiper, { Pagination } from 'swiper';
+// import 'swiper/swiper-bundle.css';
+
+const dietContainer = document.querySelector('#diet-slider');
+
+let dietSlider;
+
+Swiper.use([Pagination]);
+
+function mobileDietSwiper() {
+  if (window.innerWidth <= 991 && dietContainer.dataset.mobile === 'false') {
+    dietSlider = new Swiper(dietContainer, {
+      slidesPerView: 1,
+      loop: true,
+      spaceBetween: 10,
+      slideClass: 'diet-card',
+      pagination: {
+        el: '.swiper-pagination.diet-pagination',
+        // type: 'bullets',
+      },
+
+      breakpoints: {
+        576: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+      },
+    });
+
+    dietContainer.dataset.mobile = 'true';
+  }
+
+  if (window.innerWidth >= 992) {
+    dietContainer.dataset.mobile = 'false';
+
+    if (dietContainer.classList.contains('swiper-initialized')) {
+      dietSlider.destroy();
+    }
+  }
+}
+
+mobileDietSwiper();
+
+window.addEventListener('resize', () => {
+  mobileDietSwiper();
+});
